3001bc4ea0d137567fddfad45b43c9ca30634633
serj2626 <serj2626@mail.ru>
2024-11-15 23:48:19 +0300

    refactor: added new modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "pinia-plugin-persistedstate/nuxt",
+    "@samk-dev/nuxt-vcalendar",
+    "@nuxt/ui",
+    "nuxt-swiper",
+  ],
+});
